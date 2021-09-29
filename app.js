666df5c0277ef942ad0aafbcef6166d5c6423571
Justyna Gabiga <jot.gabiga@gmail.com>
2021-09-29 19:55:10 +0200

    allow origin

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const mongoose = require('mongoose');
 app.use(express.json());
 app.use('/books', booksRouter);
 
+//Allow origin Acces origin and methods..
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin",'https://bookcrossing-api.herokuapp.com/books');
+    res.header("Access-Control-Allow-Credentials",true);
+    res.header("Access-Control-Allow-Methods",POST, GET, PUT, DELETE, OPTIONS);
+    // res.header("Access-Control-Allow-Headers");
+    next();
+})
+
 mongoose.connect(`mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@bookcrossingcluster.mqqso.mongodb.net/bookcrossingCluster?retryWrites=true&w=majority`)
 .then(()=>console.log('Connected to MongoDB..'))
 .catch( err => console.error('Could not connect to MongoDB..',err))
