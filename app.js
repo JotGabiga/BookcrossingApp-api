f091ee35d7bbb218fd3cc664434b0da8cf41604a
Justyna Gabiga <jot.gabiga@gmail.com>
2021-09-29 20:08:32 +0200

    allow origin modification

--- app.js
+++ app.js
@@ -7,19 +7,20 @@ const mongoose = require('mongoose');
 app.use(express.json());
 app.use('/books', booksRouter);
 
-//Allow origin Acces origin and methods..
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin",'*');
-    res.header("Access-Control-Allow-Credentials",true);
-    res.header("Access-Control-Allow-Methods",POST, GET, PUT, DELETE, OPTIONS);
-    // res.header("Access-Control-Allow-Headers");
-    next();
-})
+
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@bookcrossingcluster.mqqso.mongodb.net/bookcrossingCluster?retryWrites=true&w=majority`)
 .then(()=>console.log('Connected to MongoDB..'))
 .catch( err => console.error('Could not connect to MongoDB..',err))
 
+//Allow origin Acces origin and methods..
+app.use(function(req,res,next){
+    res.setHeader("Access-Control-Allow-Origin",'*');
+    res.setHeader("Access-Control-Allow-Credentials",true);
+    res.setHeader("Access-Control-Allow-Methods",POST, GET, PUT, DELETE, OPTIONS);
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+    next();
+})
 
 
 //PORT
