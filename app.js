2981e752039297d807aaf4383ced7dd76aad3320
Justyna Gabiga <jot.gabiga@gmail.com>
2022-01-11 16:47:13 +0100

    cors modification

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ const tagsRouter = require('./controllers/tagsController');
 const usersRouter = require('./controllers/usersController');
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require ('cors');
-const app = express();
+const corsMiddleware = require ('./cors');
 
-app.use(cors()); 
-app.use(express.json());
-app.use('/books', booksRouter);
-app.use('/tags',tagsRouter);
-app.use('/user',usersRouter);
+
+const app = express();
+app.options('*',corsMiddleware);
+app.use(corsMiddleware);
+
+// app.use(cors()); 
+// app.use(express.json());
+// app.use('/books', booksRouter);
+// app.use('/tags',tagsRouter);
+// app.use('/user',usersRouter);
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@bookcrossingcluster.mqqso.mongodb.net/bookcrossingCluster?retryWrites=true&w=majority`)
 .then(()=>console.log('Connected to MongoDB..'))
 .catch( err => console.error('Could not connect to MongoDB..',err))
-
+ 
 //PORT
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ cors/index.js
@@ -0,0 +1,7 @@
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'https://bookcrossing-328121.web.app',
+    optionSuccessStatus: 200,
+}
+module.exports = cors(corsOptions);
