33e512394b3b5bd93dd402a15144fb3125e3a938
Justyna Gabiga <jot.gabiga@gmail.com>
2022-01-11 16:31:22 +0100

    cors changed

--- app.js
+++ app.js
@@ -5,19 +5,14 @@ const tagsRouter = require('./controllers/tagsController');
 const usersRouter = require('./controllers/usersController');
 const express = require('express');
 const mongoose = require('mongoose');
-
-
-const corsMiddleware = require ('./cors');
-
+const cors = require ('cors');
 const app = express();
-// app.options('*',corsMiddleware);
-app.use(corsMiddleware);
 
-// app.use(cors({origin:'*'})); 
-// app.use(express.json());
-// app.use('/books', booksRouter);
-// app.use('/tags',tagsRouter);
-// app.use('/user',usersRouter);
+app.use(cors()); 
+app.use(express.json());
+app.use('/books', booksRouter);
+app.use('/tags',tagsRouter);
+app.use('/user',usersRouter);
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@bookcrossingcluster.mqqso.mongodb.net/bookcrossingCluster?retryWrites=true&w=majority`)
 .then(()=>console.log('Connected to MongoDB..'))
--- cors/index.js
+++
@@ -1,7 +0,0 @@
-const cors = require('cors');
-
-const corsOptions = {
-    origin: 'https://bookcrossing-328121.web.app',
-    optionSuccessStatus: 200,
-}
-module.exports = cors(corsOptions);
