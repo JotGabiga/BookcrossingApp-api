2feaf84c551de20b97f54e57d95ab9c7c1fa8524
Justyna Gabiga <jot.gabiga@gmail.com>
2022-01-11 22:05:16 +0100

    cors modified

--- app.js
+++ app.js
@@ -11,33 +11,23 @@ const mongoose = require('mongoose');
 const app = express();
 
 const cors = require('cors');
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Request-Headers", "*");
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS, PUT, PATCH.');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,X-HTTP-Method-Override, Content-Type, Accept, Authorization");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-//   });
+app.use(cors());
 
-// const corsMiddleware = require ('./cors');
-// app.use(corsMiddleware);
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
-    );
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "https://bookcrossing-328121.web.app/");
+    res.header("Access-Control-Request-Headers", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS, PUT, PATCH');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,X-HTTP-Method-Override, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS, PUT, PATCH');
     next();
   });
 
-  
-app.use(cors());
+// const corsMiddleware = require ('./cors');
+// app.use(corsMiddleware);
 app.use(express.json());
 app.use('/books', booksRouter);
 app.use('/tags',tagsRouter);
