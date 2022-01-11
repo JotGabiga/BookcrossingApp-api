bceb16f297f800d18fecaa2cb67c6be8f310b5fb
Justyna Gabiga <jot.gabiga@gmail.com>
2022-01-11 17:02:54 +0100

    cors changed

--- cors/index.js
+++ cors/index.js
@@ -3,6 +3,6 @@ const cors = require('cors');
 const corsOptions = {
     origin: 'https://bookcrossing-328121.web.app',
     optionSuccessStatus: 200,
-    methods: "GET, PUT, PATCH, DELETE, POST"
+    methods: "GET, PUT, PATCH, DELETE, POST, OPTIONS"
 }
 module.exports = cors(corsOptions);
