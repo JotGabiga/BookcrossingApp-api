32ccf22113a0ee6740596163e9d095dc260da9c0
Justyna Gabiga <jot.gabiga@gmail.com>
2022-01-11 17:40:38 +0100

    cors modified

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cors = require('cors');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Request-Headers", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS, PUT, PATCH');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,X-HTTP-Method-Override, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
     next();
